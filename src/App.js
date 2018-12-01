6c1d5824fd52ba497041d7105e44c3a3438d8128
abhaydee <abhaygirish1997@gmail.com>
2018-12-01 12:43:38 +0530

    changes to app.js and added Rank

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
+import Rank from './components/Rank/Rank.js';
+import Particles from 'react-particles-js';
 
+
+const particlesoptions = {
+  particles: {
+
+    number: {
+      value: 220,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles"
+          params={particlesoptions} />
         <Logo />
         <Navigation />
         <ImageLinkForm />
+        <Rank />
       </div>
     );
   }
